a252bede71ca3ac9beb9d16ddc62818c6e69c77b
vijay <vd661163@gmail.com>
2023-08-15 13:15:43 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { AdminsComponent } from './layouts/admins/admins.component';
+import { AuthsComponent } from './layouts/auths/auths.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  {
+    path: '',
+    component: AdminsComponent,
+    // canActivate: [AuthGuard],
+    children: [{
+      path: '',
+      loadChildren: () => import("./../app/layouts/admins/admins.module").then(e => e.AdminsModule)
+    }]
+  },
+  {
+    path: '',
+    component: AuthsComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import("./../app/layouts/auths/auths.module").then(e => e.AuthsModule)
+    }]
+  },
+  {
+    path: '**',
+    redirectTo: 'users'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
